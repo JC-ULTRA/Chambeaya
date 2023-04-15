8d8a6e438e83465a56a5610615238da533394d08
juan carlos ramos <jcramosc@desarrollo-utrasist.com.mx>
2023-04-14 18:19:03 -0600

    nuevo commit

--- local-database/rundb.sh
+++
@@ -1,8 +0,0 @@
-RUTA=/home/ubuntu/development/local-database
-docker run -d \
--p 3308:3306 \
---name datos \
--v $RUTA/data:/var/lib/mysql \
--v $RUTA/scripts:/scripts \
--e MYSQL_ROOT_PASSWORD=garellano \
-mariadb
